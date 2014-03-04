b3b1a0ded99914d0a7efa3571f8d94d26d81f082
shellygr <shelly6.g@gmail.com>
2014-03-04 21:01:29 +0200

    Contd.

---
+++ AdX/adx-agnet/tau/tac/adx/agents/oos/PublisherStats.java
@@ -0,0 +1,39 @@
+package tau.tac.adx.agents.oos;
+
+public class PublisherStats {
+
+	private int popularity;
+	private int videoOrientation;
+	private int textOrientation;
+	
+	// TODO: check if orientation could be null?
+	public PublisherStats(int popularity, Integer videoOrientation, Integer textOrientation) {
+		this.popularity = popularity;
+		this.videoOrientation = videoOrientation;
+		this.textOrientation = textOrientation;
+	}
+	
+	public int getPopularity() {
+		return popularity;
+	}
+	
+	public void setPopularity(int popularity) {
+		this.popularity = popularity;
+	}
+	
+	public int getVideoOrientation() {
+		return videoOrientation;
+	}
+	
+	public void setVideoOrientation(int videoOrientation) {
+		this.videoOrientation = videoOrientation;
+	}
+	
+	public int getTextOrientation() {
+		return textOrientation;
+	}
+	
+	public void setTextOrientation(int textOrientation) {
+		this.textOrientation = textOrientation;
+	}	
+}
